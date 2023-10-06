9ab02f0525f6016a8c9018ffe684294b4d9b6e24
Osama Al-Eryani <oeryania@gmail.com>
2023-10-06 13:25:47 -0400

    Realized that subsetting to the two days is needed for both plots, not just the second plot.

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
 # load data subset if not loaded
 if (!exists("myd")) {
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "data.zip"); unzip("data.zip");
-    myd <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?")
+    myd <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?") %>%
+        mutate(DateTime = as.POSIXct(strptime(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")), .keep = "unused", .before = 1) %>%
+        subset(DateTime >= "2007-02-01" & DateTime < "2007-02-03")
 }
 
 # open graphic device
--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 # load data subset if not loaded
-if (!exists("myds")) {
+if (!exists("myd")) {
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "data.zip"); unzip("data.zip");
-    myds <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?") %>%
+    myd <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?") %>%
         mutate(DateTime = as.POSIXct(strptime(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")), .keep = "unused", .before = 1) %>%
         subset(DateTime >= "2007-02-01" & DateTime < "2007-02-03")
 }
@@ -10,10 +10,10 @@ if (!exists("myds")) {
 png("plot2.png", width = 480, height = 480)
 
 # plot, but without x-axis ticks
-plot(myds$DateTime, myds$Global_active_power, type = "l", xaxt = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(myd$DateTime, myd$Global_active_power, type = "l", xaxt = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
 
 # prepare x-axis ticks
-days <- seq(min(myds$DateTime), max(myds$DateTime), by = "day"); days_labels <- weekdays(days, abbreviate = TRUE)
+days <- seq(min(myd$DateTime), max(myd$DateTime), by = "day"); days_labels <- weekdays(days, abbreviate = TRUE)
 
 # add x-axis ticks
 axis(1, at = days, labels = days_labels)
